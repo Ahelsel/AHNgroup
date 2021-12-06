7511acd6a896bcd0f3404607b2066946106372a4
Neil Angajala <neilangajala24@gmail.com>
2021-12-05 18:00:05 -0600

    added example data

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,18 @@ fn main() {
     let mut i = 0;
     let mut j = 0;
     while i < 10 {
-        arbvec.push(Vec::new());
-        while j < 10 {    
-            arbvec[i].push(1);
-            j += 1;
-        }
+        arbvec.push(vec![0,0,0,0,0,0,0,0,0]);
+        arbvec.push(vec![1,0,0,0,2,2,5,5,5]);
+        arbvec.push(vec![1,1,1,1,2,2,5,5,5]);
+        arbvec.push(vec![1,1,1,1,1,2,2,6,6]);
+        arbvec.push(vec![0,1,1,2,2,2,6,6,6]);
+        arbvec.push(vec![0,0,3,3,3,6,6,6,0]);
+        arbvec.push(vec![0,0,3,3,3,3,3,0,0]);
+        arbvec.push(vec![0,4,4,4,4,3,0,0,0]);
+        arbvec.push(vec![0,0,0,4,4,4,0,0,0]);
+        arbvec.push(vec![0,0,0,0,4,4,0,0,0]);
         i += 1;
+
     }
     let arbitrary = CompMap {map: arbvec, numdists: 1};
     println!("{}", arbitrary.district_at(0, 0));
