11e48f2d66187a1dcad4f1b86f2c17e7636d1d66
Ahelsel <89759325+Ahelsel@users.noreply.github.com>
2021-12-08 13:54:38 -0600

    Printer now works. Outputs to printoutput.txt file.

--- src/lib.rs
+++ src/lib.rs
@@ -1,63 +1,74 @@
-mod compmap;
+    mod compmap;
+    
+    use std::fs::File;
+    use std::io::Write;
+    
+    /*
+    Display/Print function; 
+    Takes in the 2d Vector of Ints
+    Converts each row into a string.
+    A "0" will be represented by whitespace
+    Otherwise, print each line to the file, adding
+    a line break after each line.
 
-/* 
-    // Display/Print function; 
-    // Takes in the 2d Vector of Ints
-    // Converts each row into a string.
-    // A "0" will be represented by whitespace
-    // Otherwise, print each line to the file, adding
-    // a line break after each line.
-    // Prints to a file called "printoutput.txt"
-    // Takes in the 2d vec of ints
-    fn printer(input: Vec<Vec<f64>>) -> std::io::Result<()> {
-      let mut file = File::create("printoutput.txt");
-      clean_file(file);
-      let mut data: String = "";
+    Prints to a file called "printoutput.txt"
+    */
+    pub fn printer(input: Vec<Vec<i32>>) -> std::io::Result<()> {
+      let file = File::create("printoutput.txt");
+      // VVV Did this because before file was being a Result<()> instead of a File
+      let mut file = match file {
+        Ok(file) => file,
+        Err(file) => {
+          panic!();
+        }
+      };
+      file.set_len(0);
+      let mut data: String = "".to_string();
       for row in input {
         let mut row_data = row_to_string(row);
-        row_data.push('\n');
-        data.push_str(row_data);
+        if !row_data.trim().is_empty() {
+          row_data.push('\n');
+          data.push_str(&row_data);
+        }
       }
       file.write_all(data.as_bytes())?;
       file.sync_all()?;
-      Ok(());
+      Ok(())
     }
-    
-    // Takes in vector of ints, converts it to string
-    // Make sure 0 is whitespace. 
-    // If Every Character in the vector is 0, then return nothing
-    // to avoid blank lines
-    fn row_to_string() -> std::string {
-      let mut toret = "";
-      for ( int i = 0; i < input.size(); i++ ) {
-        if (input.at(i) == '0') {
-          toret.append(" ");
+
+    /*
+    Takes in vector of ints, converts it to string
+    Make sure 0 is whitespace. 
+    If Every Character in the vector is 0, then return nothing
+    to avoid blank lines
+    TODO: MAKE SURE ALL DATA IS DIGITS
+    */
+    fn row_to_string(input: Vec<i32>) -> String {
+      let mut toret: String = "".to_string();
+      for i in input {
+        if i == 0 {
+          toret.push_str(" ");
         } else {
-          toret.append(input.at(i))
+          toret.push_str(&i.to_string());
         }
       }
       return toret;
     }
 
     
-    // MAYBE
-    // Takes in the original data, and prints only the areas that changed before && after the algorithm is run, 
-    // leaving out the unaffected areas.
-    // MAYBE
-    fn print_differences() -> std::io::Result<()> {
-      todo!();
+    /*
+    Save results to different file so its not erase
+    (User has to call this command its not saved automatically);
+    */
+    /*
+    static mut NUM_SAVED_FILES: i32 = 0;
+    fn save_file() {
+      unsafe {
+        let mut filename = "save".push_str(&NUM_SAVED_FILES.to_string()).push_str(".txt");
+      }
+      fs::copy("printoutput.txt", /saves/filename);
+      unsafe {
+        NUM_SAVED_FILES += 1;
+      }
     }
-    
-    //Clears contents of the file so we can print something else on it
-    fn clean_file(input: File) -> std::io::Result<()> {
-      input.truncate();
-    }
-
-    
-    // Prints out a side by side representation
-    // of the data before and after the algorithm
-    // is called on it for restructuring  
-    fn print_sxs() -> std::io::Result<()> {
-      todo!();
-    }
-*/
+    */
