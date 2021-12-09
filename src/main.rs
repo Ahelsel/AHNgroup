33f685626c9aba0b402ae08bc98b457500f64236
vagrant <htiwary2@illinois.edu>
2021-12-08 21:54:57 -0600

    mask combiner

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,10 @@ fn main() {
     
     let mut i = 0;
     let mut j = 0;
-    //while i < 10 {
+
+    // 0 = not in map
+    // 1 - 6 = districts
+
     arbvec.push(vec![0,0,0,0,0,0,0,0,0,0]);
     arbvec.push(vec![1,0,0,0,2,2,5,5,5,5]);
     arbvec.push(vec![1,1,1,1,2,2,5,5,5,6]);
@@ -20,24 +23,14 @@ fn main() {
     arbvec.push(vec![0,4,4,4,4,3,0,0,0,0]);
     arbvec.push(vec![0,0,0,4,4,4,0,0,0,0]);
     arbvec.push(vec![0,0,0,0,4,4,0,0,0,0]);
-        // 0 = not in map
-        // 1 - 6 = districts
-        //i += 1;
-
-    //}
 
     let mut arbitrary = CompMap {map: arbvec, numdists: 6, masks: HashMap::new()};
     arbitrary.generate_masks();
-    let first_vec = arbitrary.masks.get(&3).unwrap();
+    let first_vec = arbitrary.mask_combiner(1, 3);
     for i in 0..(first_vec.len()) {
         for j in 0..first_vec[i].len() {
             print!("{}", first_vec[i][j]);
         }
         println!();
-    }
-
-    match arbitrary.district_at(0, 0) {
-        Ok(k) => println!("{}", k),
-        Err(()) => println!("{}", "my bad bro")
     }
 }
